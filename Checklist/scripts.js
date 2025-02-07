e96f6cdd67ec19329230ea7296f6c0f1f6a2150a
Denison1298 <denisonbrito01@gmail.com>
2025-02-07 11:19:15 -0400

    Update scripts.js

--- Checklist/scripts.js
+++ Checklist/scripts.js
@@ -387,3 +387,16 @@ function toggleDiasAnteriores() {
     atualizarGrafico(ocultar);
     botao.innerText = ocultar ? 'Exibir Dias Anteriores' : 'Ocultar Dias Anteriores';
 }
+
+function enviarPatrimonio(item) {
+        let lista = document.getElementById("listaPatrimonios");
+        let novoItem = document.createElement("li");
+        novoItem.textContent = item;
+        lista.appendChild(novoItem);
+        alert(item + " enviado para Patrimônios Retirados!");
+    }
+
+    function showTabContent(tabId) {
+        let tabContent = document.getElementById(tabId);
+        tabContent.style.display = tabContent.style.display === "none" ? "block" : "none";
+    }
